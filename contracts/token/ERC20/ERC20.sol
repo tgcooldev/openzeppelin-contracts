e2652afb4be2f69acd7b9b293add24fa130f6665
tgcooldev <76828588+tgcooldev@users.noreply.github.com>
2021-07-02 19:17:43 +0200

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -37,7 +37,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
 
     uint256 private _totalSupply;
 
-    number private _deci;
+    int private _deci;
     string private _name;
     string private _symbol;
 
@@ -50,7 +50,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * All two of these values are immutable: they can only be set once during
      * construction.
      */
-    constructor(string memory name_, string memory symbol_, nummer memory deci_) {
+    constructor(string memory name_, string memory symbol_, int memory deci_) {
         _name = name_;
         _symbol = symbol_;
         _deci= deci_;
